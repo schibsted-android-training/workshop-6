2e731cdaecc3547057c253f37a1cb422921ae301
Rafa Vázquez <rafa.vazsan@gmail.com>
2016-05-17 12:32:34 +0200

    Implemented first behavior of presenter

--- app/src/main/java/net/infojobs/siestatracker/SiestaPresenter.java
+++ app/src/main/java/net/infojobs/siestatracker/SiestaPresenter.java
@@ -1,12 +1,28 @@
 package net.infojobs.siestatracker;
 
+import net.infojobs.siestatracker.domain.ObtainLastSiestaDateInteractor;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 public class SiestaPresenter {
 
-    public void initialize(View view) {
+    private final ObtainLastSiestaDateInteractor obtainLastSiestaDateInteractor;
 
+    public SiestaPresenter(ObtainLastSiestaDateInteractor obtainLastSiestaDateInteractor) {
+        this.obtainLastSiestaDateInteractor = obtainLastSiestaDateInteractor;
+    }
+
+    public void initialize(View view) {
+        Date date = obtainLastSiestaDateInteractor.obtainLastSiesta();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm, dd/MM/yyyy", new Locale("es", "ES"));
+        String dateText = dateFormat.format(date);
+        view.showLastSiestaDate(dateText);
     }
 
     interface View {
 
+        void showLastSiestaDate(String lastDate);
     }
 }
---
+++ app/src/test/java/net/infojobs/siestatracker/SiestaPresenterTest.java
@@ -0,0 +1,40 @@
+package net.infojobs.siestatracker;
+
+import net.infojobs.siestatracker.domain.ObtainLastSiestaDateInteractor;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SiestaPresenterTest {
+
+    @Mock
+    SiestaPresenter.View view;
+    @Mock
+    ObtainLastSiestaDateInteractor obtainLastSiestaDateInteractor;
+
+    @Test
+    public void should_show_date_as_text_when_date_received() throws Exception {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.DAY_OF_MONTH, 28);
+        calendar.set(Calendar.YEAR, 2016);
+        calendar.set(Calendar.MONTH, Calendar.FEBRUARY);
+        calendar.set(Calendar.HOUR_OF_DAY, 21);
+        calendar.set(Calendar.MINUTE, 7);
+        Date lastSiestaDate = new Date(calendar.getTimeInMillis());
+        when(obtainLastSiestaDateInteractor.obtainLastSiesta()).thenReturn(lastSiestaDate);
+
+        SiestaPresenter presenter = new SiestaPresenter(obtainLastSiestaDateInteractor);
+        presenter.initialize(view);
+
+        verify(view).showLastSiestaDate("21:07, 28/02/2016");
+    }
+}
