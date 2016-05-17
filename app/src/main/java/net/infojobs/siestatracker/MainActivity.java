f187677761e1141bd834039c9b39d525d40fbafd
Rafa Vázquez <rafa.vazsan@gmail.com>
2016-05-17 11:09:57 +0200

    Created activity layout

--- app/src/main/java/net/infojobs/siestatracker/MainActivity.java
+++ app/src/main/java/net/infojobs/siestatracker/MainActivity.java
@@ -2,12 +2,18 @@ package net.infojobs.siestatracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView lastDateText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lastDateText = ((TextView) findViewById(R.id.last_date_text));
+
     }
 }
